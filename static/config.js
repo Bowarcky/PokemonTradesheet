c3667e7b4ea00424ee05ee926bf334bd9104e601
bowarcky <accounts@brockway.us>
2020-01-28 20:04:08 -0500

    Personalize, but keep Anubis sheet for testing

--- static/config.js
+++ static/config.js
@@ -7,16 +7,16 @@ var spreadsheetId = "1CDe5Gs4n3QsB-IkcygJEXVJUJtijABY_ItzxT4tRScY";
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "SW-3973-5484-9884";
-var inGameName = "Anubis";
+var friendCode = "I have no friends.";
+var inGameName = "Brock";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/Zaskarel";
+var contactUrl = "";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://en.gravatar.com/userimage/95234211/d17c1393486e7c6d0845d68603d89794.jpeg";
